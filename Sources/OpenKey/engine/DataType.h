690ccf2ef63a58c4d8b8eb41488597c75b48893f
tuantm90 <tuantm90+github@hotmail.com>
2025-08-08 09:22:29 +0700

    Update DataType.h

--- Sources/OpenKey/engine/DataType.h
+++ Sources/OpenKey/engine/DataType.h
@@ -1,9 +1,9 @@
 //
 //  DataType.h
-//  OpenKey
+//  vietypekey
 //
-//  Created by Tuyen on 1/18/19.
-//  Copyright © 2019 Tuyen Mai. All rights reserved.
+//  Created by T.
+//  Copyright © 2025 T. All rights reserved.
 //
 
 #ifndef DataType_h
