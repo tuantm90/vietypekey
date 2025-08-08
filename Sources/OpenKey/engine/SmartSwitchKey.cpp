4c846dc6b4df067ce96d4ef8cef77bdc07717029
tuantm90 <tuantm90+github@hotmail.com>
2025-08-08 09:25:33 +0700

    Update SmartSwitchKey.cpp

--- Sources/OpenKey/engine/SmartSwitchKey.cpp
+++ Sources/OpenKey/engine/SmartSwitchKey.cpp
@@ -1,9 +1,9 @@
 //
 //  SmartSwitchKey.cpp
-//  OpenKey
+//  vietypekey
 //
-//  Created by Tuyen on 8/13/19.
-//  Copyright © 2019 Tuyen Mai. All rights reserved.
+//  Created by T.
+//  Copyright © 2025 T. All rights reserved.
 //
 
 #include "SmartSwitchKey.h"
