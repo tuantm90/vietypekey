28b51d2fd6e6a772ae3949c9d6c622c452594bc7
tuantm90 <tuantm90+github@hotmail.com>
2025-08-08 09:52:18 +0700

    Delete Sources directory

--- Sources/OpenKey/win32/OpenKey/OpenKey/stdafx.cpp
+++
@@ -1,9 +0,0 @@
-/*----------------------------------------------------------
-vietypekey - The Cross platform Open source Vietnamese Keyboard application.
-
-Copyright (C) 2025 tuantm90
-Github: hhttps://github.com/tuantm90/vietypekey
------------------------------------------------------------*/
-#include "stdafx.h"
-
-wchar_t _logBuffer[1024];
