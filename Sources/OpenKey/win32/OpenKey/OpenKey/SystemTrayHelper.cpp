ef42da6f6bb11dc64a39c0fd901746e41c915147
tuantm90 <tuantm90+github@hotmail.com>
2025-08-08 09:12:31 +0700

    Update SystemTrayHelper.cpp

--- Sources/OpenKey/win32/OpenKey/OpenKey/SystemTrayHelper.cpp
+++ Sources/OpenKey/win32/OpenKey/OpenKey/SystemTrayHelper.cpp
@@ -31,8 +31,8 @@ Github: hhttps://github.com/tuantm90/vie-type
 #define POPUP_MACRO_TABLE 990
 
 #define POPUP_CONTROL_PANEL 1000
-#define POPUP_ABOUT_OPENKEY 1010
-#define POPUP_OPENKEY_EXIT 2000
+#define POPUP_ABOUT_vie-type 1010
+#define POPUP_vie-type_EXIT 2000
 
 #define MODIFY_MENU(MENU, COMMAND, DATA) ModifyMenu(MENU, COMMAND, \
 											MF_BYCOMMAND | (DATA ? MF_CHECKED : MF_UNCHECKED), \
@@ -62,8 +62,8 @@ map<UINT, LPCTSTR> menuData = {
 	{POPUP_QUICK_CONVERT, _T("Chuyển mã nhanh")},
 	{POPUP_MACRO_TABLE, _T("Cấu hình gõ tắt...")},
 	{POPUP_CONTROL_PANEL, _T("Bảng điều khiển...")},
-	{POPUP_ABOUT_OPENKEY, _T("Giới thiệu OpenKey")},
-	{POPUP_OPENKEY_EXIT, _T("Thoát")},
+	{POPUP_ABOUT_vie-type, _T("Giới thiệu vie-type")},
+	{POPUP_vie-type_EXIT, _T("Thoát")},
 };
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
@@ -145,11 +145,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
 			case POPUP_CONTROL_PANEL:
 				AppDelegate::getInstance()->onControlPanel();
 				break;
-			case POPUP_ABOUT_OPENKEY:
-				AppDelegate::getInstance()->onOpenKeyAbout();
+			case POPUP_ABOUT_vie-type:
+				AppDelegate::getInstance()->onvie-typeAbout();
 				break;
-			case POPUP_OPENKEY_EXIT:
-				AppDelegate::getInstance()->onOpenKeyExit();
+			case POPUP_vie-type_EXIT:
+				AppDelegate::getInstance()->onvie-typeExit();
 				break;
 			}
 			SystemTrayHelper::updateData();
@@ -225,9 +225,9 @@ void SystemTrayHelper::createPopupMenu() {
 	AppendMenu(popupMenu, MF_SEPARATOR, 0, 0);
 
 	AppendMenu(popupMenu, MF_STRING, POPUP_CONTROL_PANEL, menuData[POPUP_CONTROL_PANEL]);
-	AppendMenu(popupMenu, MF_UNCHECKED, POPUP_ABOUT_OPENKEY, menuData[POPUP_ABOUT_OPENKEY]);
+	AppendMenu(popupMenu, MF_UNCHECKED, POPUP_ABOUT_vie-type, menuData[POPUP_ABOUT_vie-type]);
 	AppendMenu(popupMenu, MF_SEPARATOR, 0, 0);
-	AppendMenu(popupMenu, MF_UNCHECKED, POPUP_OPENKEY_EXIT, menuData[POPUP_OPENKEY_EXIT]);
+	AppendMenu(popupMenu, MF_UNCHECKED, POPUP_vie-type_EXIT, menuData[POPUP_vie-type_EXIT]);
 
 	SetMenuDefaultItem(popupMenu, POPUP_CONTROL_PANEL, false);
 }
