cd57197c6d21ad362a55bafcf2ef5bc1f3a0980c
tuantm90 <tuantm90+github@hotmail.com>
2025-08-08 09:37:52 +0700

    Update OpenKeyManager.h

--- Sources/OpenKey/win32/OpenKey/OpenKey/OpenKeyManager.h
+++ Sources/OpenKey/win32/OpenKey/OpenKey/OpenKeyManager.h
@@ -1,13 +1,13 @@
 /*----------------------------------------------------------
-Vie-Type - The Cross platform Open source Vietnamese Keyboard application.
+vietypekey - The Cross platform Open source Vietnamese Keyboard application.
 
 Copyright (C) 2025 tuantm90
-Github: hhttps://github.com/tuantm90/vie-type
+Github: hhttps://github.com/tuantm90/vietypekey
 -----------------------------------------------------------*/
 #pragma once
 #include "stdafx.h"
 
-class Vie-TypeManager {
+class vietypekeyManager {
 public:
 	static unsigned short _lastKeyCode;
 	static vector<LPCTSTR>& getInputType();
